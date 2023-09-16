7191592f4fe7b226668948113709da85eab10b37
Kirikus <kirikusan@gmail.com>
2023-09-16 09:46:42 +0300

    tests: water vapor density
    Few basic tests for continuity and value on ground level.

--- tests/test_module.cpp
+++ tests/test_module.cpp
@@ -1,1 +1,24 @@
 #include "test_module.h"
+
+#include "../lib/module.h"
+
+#if !defined(WIN32)
+#define BOOST_TEST_DYN_LINK
+#endif
+#include <boost/test/unit_test.hpp>
+
+namespace tt = boost::test_tools;
+
+BOOST_AUTO_TEST_SUITE(test_rho_w)
+
+BOOST_AUTO_TEST_CASE(ground_level) { BOOST_TEST(rho_w(0, 0.5, 42) == 21); }
+
+BOOST_AUTO_TEST_CASE(rho_w_2000m) {
+  BOOST_TEST(rho_w(2000 - 1e-6) == rho_w(2000 + 1e-6), tt::tolerance(1e-6));
+}
+
+BOOST_AUTO_TEST_CASE(rho_w_8000m) {
+  BOOST_TEST(rho_w(8000 - 1e-6) == rho_w(8000 + 1e-6), tt::tolerance(1e-6));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
