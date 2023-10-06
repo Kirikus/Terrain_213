e536b8370ce9ac37485e5a5ff070efb0a0037cd4
Ishchenko Igor <igoryan11060@gmail.com>
2023-10-06 08:13:33 +0300

    ScreenAngle : Deleted from main

--- main/ScreenAngle.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCREENANGLE_H
-#define SCREENANGLE_H
-
-double ReliefFunction(double x);
-double FindCurrAngle(double x1, double y1, double x2, double y2);
-double FindScreeningAngle(double azi, double R);
-
-
-#endif // SCREENANGLE_H
