b3c8629c5d01576b3e9e505c6a6b4b7ac3f97f3d
ShatunovEvgeniy <shatunov868@gmail.com>
2023-10-16 16:04:26 +0300

    feat(map): Add image to visibility map
    Add image to visibility map in MainWindow constructor

--- main/mainwindow.cpp
+++ main/mainwindow.cpp
@@ -1,3 +1,5 @@
+#include "qcustomplot.h"
+
 #include "mainwindow.h"
 
 #include "./ui_mainwindow.h"
@@ -5,6 +7,10 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+
+  QPixmap pix("srs/img/map.jpg");
+  ui->visibility_map->setBackground(pix);
+  ui->visibility_map->setBackgroundScaledMode(Qt::IgnoreAspectRatio);
 }
 
 MainWindow::~MainWindow() { delete ui; }
