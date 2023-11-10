f081086a2bbaa30e0c279ba8807d6f90939c1367
Ishchenko Igor <igoryan11060@gmail.com>
2023-11-10 13:46:30 +0300

    feat(math): Add class CartesianPoint

--- lib/point.h
+++ lib/point.h
@@ -12,3 +12,23 @@ protected:
 public:
     Point() = default;
 };
+
+class CartesianPoint : Point
+{
+private:
+    double x;
+    double y;
+    double h;
+
+public:
+
+    CartesianPoint() = default;
+    CartesianPoint(double x, double y, double h)
+        :x{x}, y{y}, h{h}
+    {}
+
+    virtual double get_x() override {return x;};
+    virtual double get_y() override {return y;};
+    virtual double get_h() override {return h;};
+};
+
