16605c247db24d2abead06189020cf945d9afb4d
Ishchenko Igor <igoryan11060@gmail.com>
2023-11-10 13:46:49 +0300

    feat(math): Add class SphericPoint

--- lib/point.h
+++ lib/point.h
@@ -32,3 +32,30 @@ public:
     virtual double get_h() override {return h;};
 };
 
+class SphericPoint : Point
+{
+public:
+    double get_R() {return R;};
+    double get_phi() {return phi;};
+    double get_azimuth() {return azimuth;};
+
+    virtual double get_x() override {return center.get_x();};
+    virtual double get_y() override {return center.get_y();};
+    virtual double get_h() override {return center.get_h();};
+
+    SphericPoint() = default;
+    SphericPoint(CartesianPoint center, CartesianPoint target);
+    SphericPoint(const SphericPoint& sp);
+
+    void change_r(double new_R) {this->R = new_R;}
+    void change_azimuth(double new_azimuth) {this->azimuth = new_azimuth;}
+    void change_phi(double new_phi) {this->phi = new_phi;}
+
+private:
+    CartesianPoint center;
+    double azimuth;
+    double R;
+    double phi;
+};
+
+#endif // POINT_H
