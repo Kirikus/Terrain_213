7f5e3443207a2c8d6073c4723120c96613db8e04
Ishchenko Igor <igoryan11060@gmail.com>
2023-11-11 17:44:51 +0300

    feach(math): completed functions

--- lib/point.cpp
+++ lib/point.cpp
@@ -8,9 +8,15 @@ PointSpheric::PointSpheric(PointCartesian center, PointCartesian target)
     double xy = hypot(x, y);
 
     this->center = center;
+    this->target = target;
     this->R = sqrt(pow(x, 2) + pow(y, 2) + pow(h, 2));
 
+    //atan2 defined in the range from -180 to 180;
+    //therefore, if the angle is negative,
+    //then we will convert it to the corresponding positive
     this->azimuth = atan2(y, x) * 180 / M_PI;
+    if (this->azimuth < 0) this->azimuth += 360;
+    this->azimuth /= 180;
     this->azimuth *= M_PI;
 
     this->phi = atan(h/xy);
@@ -19,6 +25,7 @@ PointSpheric::PointSpheric(PointCartesian center, PointCartesian target)
 PointSpheric::PointSpheric(const PointSpheric& sp)
 {
     this->center = sp.center;
+    this->target = sp.target;
     this->azimuth = sp.azimuth;
     this->R = sp.R;
     this->phi = sp.phi;
