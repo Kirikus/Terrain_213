a0ae7451854ed2599b67c07925f05cebfd83d305
Ishchenko Igor <igoryan11060@gmail.com>
2023-11-11 17:48:44 +0300

    fix(math): fixed coordinate get function

--- lib/point.h
+++ lib/point.h
@@ -39,9 +39,9 @@ public:
     double get_phi() {return phi;};
     double get_azimuth() {return azimuth;};
 
-    virtual double get_x() override {return target.get_x();};
-    virtual double get_y() override {return target.get_y();};
-    virtual double get_h() override {return target.get_h();};
+    virtual double get_x() override {return target.get_x() - center.get_x();};
+    virtual double get_y() override {return target.get_y() - center.get_y();};
+    virtual double get_h() override {return target.get_h() - center.get_h();};
 
     PointSpheric() = default;
     PointSpheric(PointCartesian center, PointCartesian target);
