b58d0791b50276720dbac9d1f76c515993f79d0c
Ishchenko Igor <igoryan11060@gmail.com>
2023-11-12 11:18:16 +0300

    feach(math):
    Added a function that looks for the closing angle

--- lib/screenangle.cpp
+++ lib/screenangle.cpp
@@ -17,3 +17,37 @@ double FindCurrAngle(double x1, double y1, double z1, double x2, double y2, doub
     return atan(opposite_cathet/adjacent_cathet);
 }
 
+
+//The FindScreeningAngle function searches for the closing angle for a given approximate relief, returns the angle in degrees
+//Accepts:
+//azi - azimuth angle (specified in radians);
+//R is the radius of the closing angle search (specified in meters).
+//Traversed by a continuous relief function
+//Now azi = 0
+double FindScreeningAngle(double azi, double R)
+{
+    double rls_x = 0, rls_y = 0, rls_z = 0;
+    double dx = 0.05*cos(azi), dy = 0.05*sin(azi);
+    double curr_x = rls_x, curr_y = rls_y, curr_z = rls_z;
+    double curr_r = 0;
+    double screening_angle = 0;
+
+    while (curr_r <= R)
+    {
+        curr_x += dx;
+        curr_y += dy;
+        curr_r = sqrt(pow((curr_x - rls_x), 2) + pow((curr_y - rls_y), 2));
+
+        curr_z = ModelReliefFunction(curr_x, curr_y);
+        if (curr_z <= rls_z)
+            continue;
+
+        double curr_angle = FindCurrAngle(rls_x, rls_y, rls_z, curr_x, curr_y, curr_z);
+        if (curr_angle <= screening_angle)
+            continue;
+
+        screening_angle = curr_angle;
+    }
+
+    return screening_angle;
+}
