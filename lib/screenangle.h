8c4863bd197b604ecd0d455509ee914fff9005a1
Ishchenko Igor <igoryan11060@gmail.com>
2023-11-14 18:45:12 +0300

    chore(math): deleted FindCurrAngle

--- lib/screenangle.h
+++ lib/screenangle.h
@@ -5,7 +5,6 @@
 #include <cmath>
 
 double ModelReliefFunction(double x, double y);
-double FindCurrAngle(double x1, double y1, double z1, double x2, double y2, double z2);
 double FindScreeningAngle(PointCartesian rls, double azi, double R);
 
 #endif // SCREENANGLE_H
