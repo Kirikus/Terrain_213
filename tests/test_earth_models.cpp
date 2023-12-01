43b0b1345e2ffee9255f1b69f067d7ffc0b065ef
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-01 12:21:42 +0300

    tests(math): added tests for SphericModel

--- tests/test_earth_models.cpp
+++ tests/test_earth_models.cpp
@@ -28,4 +28,16 @@ BOOST_AUTO_TEST_CASE(test_ModelFlat_short_distance) {
     BOOST_TEST(fe.find_phi(center, target) == asin(h/r), tt::tolerance(1e-6));
 }
 
+BOOST_AUTO_TEST_CASE(test_ModelSpheric_short_distance) {
+    ModelSpheric se;
+
+    PointCartesian center(2, 2, 4);
+    PointCartesian target(60, 60, 100);
+
+    double r = 126.269986;
+    double phi = 0.8637309;
+
+    BOOST_TEST(se.find_r(center, target) == r, tt::tolerance(1e-6));
+    BOOST_TEST(se.find_phi(center, target) == phi, tt::tolerance(1e-6));
+}
 BOOST_AUTO_TEST_SUITE_END()
