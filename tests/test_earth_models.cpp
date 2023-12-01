13ad4a7030504024d8860fed47de4df27cae0477
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-01 12:21:22 +0300

    tests(math); added tests for FlatModel

--- tests/test_earth_models.cpp
+++ tests/test_earth_models.cpp
@@ -13,6 +13,19 @@ namespace tt = boost::test_tools;
 
 BOOST_AUTO_TEST_SUITE(test_earth_models)
 
+BOOST_AUTO_TEST_CASE(test_ModelFlat_short_distance) {
+    ModelFlat fe;
 
+    PointCartesian center(2, 2, 4);
+    PointCartesian target(60, 60, 100);
+
+    double x = target.get_x() - center.get_x();
+    double y = target.get_y() - center.get_y();
+    double h = target.get_h() - center.get_h();
+
+    double r = sqrt(pow(x, 2) + pow(y, 2) + pow(h, 2));
+    BOOST_TEST(fe.find_r(center, target) == r, tt::tolerance(1e-6));
+    BOOST_TEST(fe.find_phi(center, target) == asin(h/r), tt::tolerance(1e-6));
+}
 
 BOOST_AUTO_TEST_SUITE_END()
