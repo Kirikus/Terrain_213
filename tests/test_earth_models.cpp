4da62f34aebe0b355bda3654f10ac858a90acec4
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-01 11:55:05 +0300

    feat(math): prepared file for tests

--- tests/test_earth_models.cpp
+++ tests/test_earth_models.cpp
@@ -1,1 +1,18 @@
 #include "test_earth_models.h"
+
+#include <QApplication>
+
+#include "../lib/earth_models.h"
+
+#if !defined(WIN32)
+#define BOOST_TEST_DYN_LINK
+#endif
+#include <boost/test/unit_test.hpp>
+
+namespace tt = boost::test_tools;
+
+BOOST_AUTO_TEST_SUITE(test_earth_models)
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
