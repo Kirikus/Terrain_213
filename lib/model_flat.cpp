492b19d776578163a47b76f0c1982a26b4ab1808
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-01 16:29:32 +0300

    refactor(math): moved model flat
    Moved model flat into another file

---
+++ lib/model_flat.cpp
@@ -0,0 +1,22 @@
+#include "models_earth.h"
+
+//This function calculates distance between center and target
+double ModelFlat::find_r(PointCartesian center, PointCartesian target)
+{
+    double x = target.get_x() - center.get_x();
+    double y = target.get_y() - center.get_y();
+    double h = target.get_h() - center.get_h();
+
+    double r = std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(h, 2));
+    return r;
+}
+
+//This function calculates elevation angle between center and target
+double ModelFlat::find_phi(PointCartesian center, PointCartesian target)
+{
+    double h = target.get_h() - center.get_h();
+    double r = find_r(center, target);
+
+    double phi = std::asin(h/r);
+    return phi;
+}
