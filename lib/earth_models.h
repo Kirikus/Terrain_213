e835827637cdb610606e1f05a183b12ef428c074
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-01 09:37:19 +0300

    feat(math): added functions parameters

--- lib/earth_models.h
+++ lib/earth_models.h
@@ -5,26 +5,26 @@
 
 class ModelEarth
 {
-    virtual double find_r() = 0;
-    virtual double find_phi() = 0;
+    virtual double find_r(PointCartesian center, PointCartesian target) = 0;
+    virtual double find_phi(PointCartesian center, PointCartesian target) = 0;
 };
 
 class ModelFlat: ModelEarth
 {
-    virtual double find_r() override {return 0;};
-    virtual double find_phi() override {return 0;};
+    virtual double find_r(PointCartesian center, PointCartesian target) override {return 0;};
+    virtual double find_phi(PointCartesian center, PointCartesian target) override {return 0;};
 };
 
 class ModelSpheric: ModelEarth
 {
-    virtual double find_r() override {return 0;};
-    virtual double find_phi() override {return 0;};
+    virtual double find_r(PointCartesian center, PointCartesian target) override {return 0;};
+    virtual double find_phi(PointCartesian center, PointCartesian target) override {return 0;};
 };
 
 class ModelEffectiveRadius: ModelEarth
 {
-    virtual double find_r() override {return 0;};
-    virtual double find_phi() override {return 0;};
+    virtual double find_r(PointCartesian center, PointCartesian target) override {return 0;};
+    virtual double find_phi(PointCartesian center, PointCartesian target) override {return 0;};
 };
 
 #endif // EARTH_MODELS_H
