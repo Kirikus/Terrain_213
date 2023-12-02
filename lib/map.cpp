a7cd359e8550b1ed65fb2e3c2cd86e856daf5068
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-02 15:35:48 +0300

    chore(math): renamed new_d

--- lib/map.cpp
+++ lib/map.cpp
@@ -1,8 +1,8 @@
 #include "map.h"
 
-double Map1d::height(double new_d)
+double Map1d::height(double d)
 {
     PointSpheric p(rls, target);
-    p.change_d(new_d);
+    p.change_d(d);
     return data->h(p);
 }
