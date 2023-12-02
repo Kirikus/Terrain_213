26b2ff51e3617bfac03448bdba0518a67bc7cd7a
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-02 18:14:50 +0300

    feat(math): improved constant
    improved constant dielictric permittivity class

--- lib/dielectric_permittivity.h
+++ lib/dielectric_permittivity.h
@@ -14,7 +14,12 @@ public:
 class Constant: public DielectricPermittivity
 {
 public:
+    Constant(double coeff)
+        :coeff(coeff)
+    {}
+
     double dielectricPermittivity(PointCartesian p) override {return coeff;};
+private:
     double coeff;
 };
 
