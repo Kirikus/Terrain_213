5689cf0ad46878e1f5121ea7d8b7fbbba9d4a14c
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-02 18:18:21 +0300

    tests(math): added simple test

--- tests/test_map.cpp
+++ tests/test_map.cpp
@@ -8,9 +8,26 @@
 #include <boost/test/unit_test.hpp>
 
 namespace tt = boost::test_tools;
+namespace EL = Elevation;
+namespace VG = Vegetation;
+namespace DP = DielectricPermittivity;
 
 BOOST_AUTO_TEST_SUITE(test_map)
 
+BOOST_AUTO_TEST_CASE(simple_map) {
+    PointCartesian point1(2, 2, 0);
+    PointCartesian point2(2, 2, 2);
+
+    EL::Plain plain;
+    VG::None veg;
+    DP::Constant dp(1);
+
+    Map map(&plain, &veg, &dp);
+    BOOST_TEST(point1.get_h() == map.h(point1), tt::tolerance(1e-6));
+    BOOST_TEST(point2.get_h() != map.h(point1), tt::tolerance(1e-6));
+    BOOST_TEST(map.v(point1) == 0, tt::tolerance(1e-6));
+    BOOST_TEST(map.dp(point1) == 1, tt::tolerance(1e-6));
+}
 
 
 BOOST_AUTO_TEST_SUITE_END()
