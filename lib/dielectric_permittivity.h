100dcc8a7bb8e3465003e0becbf60c85b2dc9c68
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-02 14:33:07 +0300

    fix(math): removed complex

--- lib/dielectric_permittivity.h
+++ lib/dielectric_permittivity.h
@@ -2,27 +2,25 @@
 #define DIELECTRIC_PERMITTIVITY_H
 
 #include "point.h"
-#include <complex>
-using complex = std::complex<double>;
 
 namespace DielectricPermittivity
 {
 class DielectricPermittivity
 {
 protected:
-    virtual complex dielectricPermittivity(PointSpheric p) = 0;
+    virtual double dielectricPermittivity(PointSpheric p) = 0;
 };
 
 class Constant: DielectricPermittivity
 {
 public:
-    complex dielectricPermittivity(PointSpheric p) override;
+    double dielectricPermittivity(PointSpheric p) override;
 };
 
 class GeoData: DielectricPermittivity
 {
 public:
-    complex dielectricPermittivity(PointSpheric p) override;
+    double dielectricPermittivity(PointSpheric p) override;
 };
 }
 
--- lib/vegetation.h
+++ lib/vegetation.h
@@ -2,34 +2,31 @@
 #define VEGETATION_H
 
 #include "point.h"
-#include <complex>
-
-using complex = std::complex<double>;
 
 namespace Vegetation
 {
 class Vegetation
 {
 protected:
-    virtual complex vegetation(PointSpheric p) = 0;
+    virtual double vegetation(PointSpheric p) = 0;
 };
 
 class None: Vegetation
 {
 public:
-    complex vegetation(PointSpheric p) override;
+    double vegetation(PointSpheric p) override;
 };
 
 class Constant: Vegetation
 {
 public:
-    complex vegetation(PointSpheric p) override;
+    double vegetation(PointSpheric p) override;
 };
 
 class GeoData: Vegetation
 {
 public:
-    complex vegetation(PointSpheric p) override;
+    double vegetation(PointSpheric p) override;
 };
 }
 
