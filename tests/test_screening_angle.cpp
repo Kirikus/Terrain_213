5126aa2ce39e23f3f81a6002be4cb9c1a61175f2
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-05 17:32:16 +0300

    refactor(math): remade screening angle search func

--- tests/test_screening_angle.cpp
+++ tests/test_screening_angle.cpp
@@ -19,11 +19,14 @@ BOOST_AUTO_TEST_CASE(ModelReliefFunction_test) {
 }
 
 PointCartesian rls1(0, 0, 0);
+PointCartesian target1(1, 0, 0);
+
+PointSpheric sp1(rls1, target1);
 
 BOOST_AUTO_TEST_CASE(FindScreeningAngle_simple_test) {
-    BOOST_TEST(FindScreeningAngle(rls1, 0, hypot(10, 5)) == atan(5./10), tt::tolerance(1e-4));
-    BOOST_TEST(FindScreeningAngle(rls1, 0, hypot(15, 20)) == atan(15./20), tt::tolerance(1e-4));
-    BOOST_TEST(FindScreeningAngle(rls1, 0, 30) == atan(15./20), tt::tolerance(1e-4));
+    BOOST_TEST(FindScreeningAngle(sp1, hypot(10, 5)) == atan(5./10), tt::tolerance(1e-4));
+    BOOST_TEST(FindScreeningAngle(sp1, hypot(15, 20)) == atan(15./20), tt::tolerance(1e-4));
+    BOOST_TEST(FindScreeningAngle(sp1, 30) == atan(15./20), tt::tolerance(1e-4));
 }
 
 PointCartesian rls2(10, 0, ModelReliefFunction(10, 0));
@@ -31,13 +34,31 @@ PointCartesian rls3(17, 4, ModelReliefFunction(17, 4));
 PointCartesian rls4(23, 4, ModelReliefFunction(23, 4));
 PointCartesian rls5(35, 0, ModelReliefFunction(35, 0));
 
+PointCartesian target2(11, 0, ModelReliefFunction(10, 0));
+PointCartesian target3(18, 4, ModelReliefFunction(17, 4));
+PointCartesian target4(24, 4, ModelReliefFunction(23, 4));
+PointCartesian target5(36, 0, ModelReliefFunction(35, 0));
+
+
+PointSpheric sp2(rls2, target2);
+PointSpheric sp3(rls3, target3);
+PointSpheric sp4(rls4, target4);
+PointSpheric sp5(rls5, target5);
+
 BOOST_AUTO_TEST_CASE(FindScreeningAngle_on_mountain_test) {
-    BOOST_TEST(FindScreeningAngle(rls2, 0, 20) == atan(1), tt::tolerance(1e-6));
-    BOOST_TEST(FindScreeningAngle(rls2, 0, 30) == atan(1), tt::tolerance(1e-6));
-    BOOST_TEST(FindScreeningAngle(rls2, 0, 0) == 0., tt::tolerance(1e-6));
-    BOOST_TEST(FindScreeningAngle(rls3, 0, hypot(3, 1)) == 0.5403724, tt::tolerance(1e-6));
-    BOOST_TEST(FindScreeningAngle(rls4, M_PI, hypot(3, 1)) == 0.5403724, tt::tolerance(1e-6));
-    BOOST_TEST(FindScreeningAngle(rls5, M_PI, hypot(20, 15)) == atan(1), tt::tolerance(1e-6));
+    sp2.change_azimuth(0);
+    sp3.change_azimuth(0);
+    sp4.change_azimuth(M_PI);
+    sp5.change_azimuth(M_PI);
+
+    double incline3_4 = 0.48568956;
+
+    BOOST_TEST(FindScreeningAngle(sp2, 20) == atan(1), tt::tolerance(1e-6));
+    BOOST_TEST(FindScreeningAngle(sp2, 30) == atan(1), tt::tolerance(1e-6));
+    BOOST_TEST(FindScreeningAngle(sp2, 0) == 0., tt::tolerance(1e-6));
+    BOOST_TEST(FindScreeningAngle(sp3, hypot(3, 1)) == incline3_4, tt::tolerance(1e-6));
+    BOOST_TEST(FindScreeningAngle(sp4, hypot(3, 1)) == incline3_4, tt::tolerance(1e-6));
+    BOOST_TEST(FindScreeningAngle(sp5, hypot(20, 15)) == atan(1), tt::tolerance(1e-6));
 }
 
 
