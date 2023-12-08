86a84a10c409faf357e02bae8d8e97991206da0d
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-08 10:35:17 +0300

    feat(math): added ReflectionMultiplier

--- lib/reflection_multiplier.h
+++ lib/reflection_multiplier.h
@@ -6,4 +6,16 @@
 #include "elevation_reflection_multiplier.h"
 #include "frenel_multiplier.h"
 #include "vegetation_reflection_multiplier.h"
+
+using complex = std::complex<double>;
+
+namespace ReflectionCoefficients
+{
+class ReflectionMultiplier
+{
+public:
+    complex reflection_multiplier();
+};
+}
+
 #endif // REFLECTION_MULTIPLIER_H
