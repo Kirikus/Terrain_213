169b1ff2c0dc37846264ab8ac2f086930021985b
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-08 10:32:13 +0300

    feat(math): added FrenelReflectionMultiplier

--- lib/frenel_multiplier.h
+++ lib/frenel_multiplier.h
@@ -7,4 +7,13 @@
 
 using complex = std::complex<double>;
 
+namespace ReflectionCoefficients
+{
+class FrenelReflectionMultiplier
+{
+public:
+    complex horizontal_polarization(Map* map, PointSpheric sp, double incidence_angle);
+    complex vertical_polarization(Map* map, PointSpheric sp, double incidence_angle);
+};
+}
 #endif // FRENEL_MULTIPLIER_H
