34ecf63a118a1fb88dbff71e60e016abbb6d5f03
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-08 10:31:46 +0300

    feat(math): added frenel_coeff function

--- lib/elevation_reflection_multiplier.cpp
+++ lib/elevation_reflection_multiplier.cpp
@@ -1,1 +1,19 @@
 #include "elevation_reflection_multiplier.h"
+
+namespace RP = ReflectionCoefficients;
+
+//  This function calculates Frenel Coefficient in case of uneven surface
+//  function accepts:
+//  1) incidence angle in radians.
+//  2) wave lenght in meters;
+//  3) The COE of the height spread from the average value.
+//  function returns:
+//  1) frenel coefficient;
+//  formula (2.13) on the page 48
+
+double RP::ElevationReflectionMultiplier::frenel_coefficient(double incidence_angle, double wave_l, double sko)
+{
+    double teta = incidence_angle * 180 / M_PI;
+    double coef = exp((-1./2) * pow((4 * M_PI * sko * sin(teta) / wave_l), 2));
+    return coef;
+}
