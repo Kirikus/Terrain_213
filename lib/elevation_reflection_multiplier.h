ba07d62d6adb32fca02bf0044c8ef8635991c32c
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-08 10:31:24 +0300

    feat(math): added ElevationReflectionMultiplier

--- lib/elevation_reflection_multiplier.h
+++ lib/elevation_reflection_multiplier.h
@@ -4,4 +4,13 @@
 #include <cmath>
 #include <complex>
 
+namespace ReflectionCoefficients
+{
+class ElevationReflectionMultiplier
+{
+public:
+    double frenel_coefficient(double incidence_angle, double wave_l, double sko);
+};
+}
+
 #endif // ELEVATION_REFLECTION_MULTIPLIER_H
