7ec70c694a3443a4f47586249f4b68eafe84a37f
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-10 19:01:59 +0300

    chore(math): added todo

--- lib/reflection_multiplier.cpp
+++ lib/reflection_multiplier.cpp
@@ -8,4 +8,4 @@ std::complex<double> RP::ReflectionMultiplier::reflection_multiplier(Map* map, P
     std::complex<double> fr_c = FrenelCoeff->horizontal_polarization(map, sp, incidence_angle);
     double vg_c = VegetationCoeff->vegetation_coeff(incidence_angle, a, b, wave_l);
     return el_c * fr_c * vg_c;
-}
+} // todo
