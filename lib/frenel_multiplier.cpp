e871801a5e220f1aa8776e53165dca760b808b6e
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-11 16:13:44 +0300

    refactor(math): changed map usage

--- lib/frenel_multiplier.cpp
+++ lib/frenel_multiplier.cpp
@@ -15,8 +15,8 @@ namespace RP = ReflectionCoefficients;
 
 std::complex<double> RP::FrenelReflectionMultiplier::horizontal_polarization(Map* map, PointSpheric sp, double incidence_angle, double wave_l, double conductivity)
 {
-    Map1d map1d(map, sp.get_center(), sp.get_target());
-    std::complex<double> eps = (map1d.dielectric_permittivity(sp.get_d()), -60 * wave_l * conductivity);
+    PointCartesian relief_dot = sp.get_target();
+    std::complex<double> eps = (map->dp(&relief_dot), -60 * wave_l * conductivity);
     std::complex<double> numerator = std::sin(incidence_angle) - std::sqrt(eps - std::pow(std::cos(incidence_angle), 2));
     std::complex<double> denumerator = std::sin(incidence_angle) + std::sqrt(eps - std::pow(std::cos(incidence_angle), 2));
     std::complex<double> frenel_coeff = numerator / denumerator;
@@ -25,8 +25,8 @@ std::complex<double> RP::FrenelReflectionMultiplier::horizontal_polarization(Map
 
 std::complex<double> RP::FrenelReflectionMultiplier::vertical_polarization(Map* map, PointSpheric sp, double incidence_angle, double wave_l, double conductivity)
 {
-    Map1d map1d(map, sp.get_center(), sp.get_target());
-    std::complex<double> eps = (map1d.dielectric_permittivity(sp.get_d()), -60 * wave_l * conductivity);
+    PointCartesian relief_dot = sp.get_target();
+    std::complex<double> eps = (map->dp(&relief_dot), - 60 * wave_l * conductivity);
     std::complex<double> numerator = eps * std::sin(incidence_angle) - std::sqrt(eps - std::pow(std::cos(incidence_angle), 2));
     std::complex<double> denumerator = eps * std::sin(incidence_angle) + std::sqrt(eps - std::pow(std::cos(incidence_angle), 2));
     std::complex<double> frenel_coeff = numerator / denumerator;
