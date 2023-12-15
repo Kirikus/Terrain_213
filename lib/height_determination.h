6b7c6c4255242aec4135aa4a78b38ce62b9c5f0d
Ishchenko Igor <igoryan11060@gmail.com>
2023-12-15 10:43:32 +0300

    feat(math): completed FindTargetHeight

--- lib/height_determination.h
+++ lib/height_determination.h
@@ -5,6 +5,6 @@
 #include "map.h"
 #include "reflection_point.h"
 
-double FindTargetHeight(Map* map, EarthModels::ModelEarth* model, PointCartesian rls, Point2d target, double r, double r1, double r2, double r_max);
+double FindTargetHeight(Map* map, EarthModels::ModelEarth* model, PointCartesian rls, Point2d target, PointCartesian* reflection_point, double r, double r1, double r2, double r_max);
 
 #endif // HEIGHT_DETERMINATION_H
